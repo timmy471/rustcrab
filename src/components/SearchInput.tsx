e2b714335a06773bd30c38a6ad3d26507c52b0eb
timmy471 <ayantunjitimilehin@gmail.com>
2024-11-07 12:02:31 +0100

    Improved SearchInput Accessiblity

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -7,9 +7,14 @@ interface SearchInputProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SearchInput: React.FC<SearchInputProps> = ({
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  error?: string;
+};
+
+const SearchInput: React.FC<InputProps> = ({
   placeholder,
   value,
+  error,
   onChange,
 }) => {
   return (
@@ -19,11 +24,14 @@ const SearchInput: React.FC<SearchInputProps> = ({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
+        aria-label={placeholder || "Search"}
+        aria-invalid={!!error}
         className="w-full p-3 pl-10 rounded-full border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-red-500"
       />
       <Search
         className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
         size={20}
+        aria-hidden="true"
       />
     </div>
   );
