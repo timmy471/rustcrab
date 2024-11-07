97df340fcd655c49bf2b3127099c1c22e3685d6d
timmy471 <ayantunjitimilehin@gmail.com>
2024-11-07 09:43:58 +0100

    Create SearchInput component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Search } from "lucide-react";
+
+interface SearchInputProps {
+  placeholder?: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <div className="mb-8 max-w-md mx-auto">
+      <div className="relative">
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className="w-full p-3 pl-10 rounded-full border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-red-500"
+        />
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+          size={20}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchInput;
